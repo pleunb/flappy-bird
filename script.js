0ebc3b269f4c652279570492179474656736be78
Pleun <74767267+pleunb@users.noreply.github.com>
2021-09-22 12:05:15 +0000

    figuring out pillars

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 var jump = false;
 var gravity = 2;
 
+// pillar
+var b1X = 200;
+var b1Y = 400;
+var b1Width = 30;
+var b1Height = 300;
+var xspeed = -5
 
+// pillar 2
+var b2X = 200;
+var b2Y = -100;
+var b2Width = 30;
+var b2Height = 300;
+var xspeed = -5
 
 function setup() {
 	createCanvas(300, 600);
@@ -12,11 +24,14 @@ var [xpos, ypos, xspeed, yspeed] = [100, 300, 5, 5];
 function draw() {
 	background(225);
 	
-	fill(0, 150, 0);
+	fill("pink");
 	ellipse(xpos, ypos, 30, 30);
-	
+
+ 	fill("blue"); 
+  rect(b1X, b1Y, b1Width, b1Height);
+	rect(b2X, b2Y, b2Width, b2Height);
   //ypos = ypos + 5
-	if(ypos >= 0 && ypos + 50 <= 600) ypos += yspeed;
+	if(ypos >= 0 && ypos + 25 <= 600) ypos += yspeed;
 }
 
 function keyPressed() {
@@ -25,10 +40,6 @@ function keyPressed() {
       jump = true;
 			yspeed = -10;
 			break;
-		case 40:
-		case 83:
-			yspeed = 2;
-			break;
 	}
 }
 
@@ -36,11 +47,7 @@ function keyReleased() {
 	switch(keyCode) {
     case 32:
       jump = false;
-			yspeed = 0;
-			break;
-		case 40:
-		case 83:
-			yspeed = 0;
+			yspeed = 5;
 			break;
 	}
 }
@@ -48,5 +55,3 @@ function keyReleased() {
 function gravity(){
   ypos = ypos + (gravity);
 }  
-
-
