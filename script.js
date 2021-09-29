eaa1a3c43be87ea85b70c2842a54627e5bef053e
Pleun <74767267+pleunb@users.noreply.github.com>
2021-09-29 12:52:32 +0000

    updated pipes so they are random heights :O

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-class Pillar{
-  constructor (x, y, w, h, vx){
+class Pipe {
+  constructor(x, y, h) {
     this.x = x;
     this.y = y;
-    this.w = w;
     this.h = h;
-    this.vx = vx;
   }
   
-  draw() {
-    fill('green')
-    rect(this.x, this.y, 30, 300)
-    this.x = this.x + this.vx
-  } 
+  drawPipe() {
+    fill("green")
+    rect(this.x, this.y, 30, this.h);
+    this.x = this.x - 5;
+  }
 }
 
 // Bird
@@ -19,37 +17,34 @@ var [xpos, ypos, yspeed] = [100, 300, 5];
 
 
 //pillars
-var pillars = [];
+var pipes = [];
 
 function setup() {
 	createCanvas(300, 600);
-
-  pillar1 = new Pillar(300, random(300, 550), 30, 300, -2);
-  pillar2 = new Pillar(300, random(-300, 0), 30, 300, -2);
-  pillars.push(pillar1);
-  pillars.push(pillar2);
 }
 
 function draw() {
-	background(225);
-  fill(255, 0, 0); //?
+  background(225);
 
-  if(frameCount % 120 == 0){
-    console.log(frameCount);
-    let newPillarBot = new Pillar(300, random(300, 550), 30, 300, -2);
-    let newPillarTop = new Pillar(300, random(-300, 0), 30, 300, -2);  
-    pillars.push(newPillarBot);
-    pillars.push(newPillarTop);
+
+  if(frameCount % 120 == 0){   
+    console.log("draw pipe!");
+
+    let randomHeight = random(height - 150)
+
+    pipes.push(new Pipe(800,0, randomHeight));
+    pipes.push(new Pipe(8  00,randomHeight + 150, 1000));
   }
+
+  pipes.forEach(p => p.drawPipe());
 
   fill("yellow");
 	ellipse(xpos, ypos, 30, 30);
   
 	if(ypos >= 0 && ypos + 25 <= 600) ypos += yspeed;
   else ypos = 300;
-
-  pillars.forEach(r => r.draw());
 }
+
 
 function keyPressed() {
 	switch(keyCode) {
