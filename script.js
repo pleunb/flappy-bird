414d39f8e780f378a95cb7ccca36b3384808f270
Martijn <74767447+MartijnC-05@users.noreply.github.com>
2021-10-06 11:42:03 +0000

    bird realistisch laten springen

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let pillars = [];
+var gravity = 0.35;
+var y = 200;
+var vy = -1.0;
+var bird;
+
+
+
 class Pillar {
   constructor(x, y, h) {
     this.x = x;
@@ -13,30 +21,35 @@ class Pillar {
 }
 
 class Bird{
-  constructor(xpos, ypos, yspeed){
-    this.xpos = xpos;
-    this.ypos = ypos;
-    this.yspeed = yspeed;
+  constructor(vy, y, gravity){
+    this.vy = vy;
+    this.y = y;
+    this.gravity = gravity;
   }
 
-  drawBird(){
-    fill("yellow");
-    ellipse(100, 300, -5);
+  draw(){
+    ellipse(150, this.y, 30, 30);
   }
+
+  move(){   
+    this.vy += this.gravity;
+    this.y += this.vy;
+    // dit hieronder doet raar met de foto als bird
+    this.y = constrain(this.y, 10, 580);
+  } 
 }
-var [xpos, ypos, yspeed] = [100, 300, 5];
+
 
 //pillars
-var pillars = [];
-
 function setup() {
 	createCanvas(800, 600);
+  bird = new Bird(-0.5, 150, 0.25);
 }
 
 function draw() {
-  background(225);
-
-  drawBird;
+  background(255);
+  bird.draw();
+  bird.move();
 
   if(frameCount % 60 == 0){   
     let randomHeight = random(height - 150)
@@ -46,32 +59,15 @@ function draw() {
   }
 
   pillars.forEach(p => p.drawPillar());
-
-  
-  fill("yellow");
-	ellipse(xpos, ypos, 30, 30);
-	if(ypos >= 0 && ypos + 25 <= 600) ypos += yspeed;
-  else ypos = 300;
 }
 
 
-function keyPressed() {
-	switch(keyCode) {
-    case 32:
-      jump = true;
-			yspeed = -10;
-			break;
-	}
+function keyPressed(){
+  if(keyCode == 32){
+    bird.vy = -7; 
+  }
 }
 
-function keyReleased() {
-	switch(keyCode) {
-    case 32:
-      jump = false;
-			yspeed = 5;
-			break;
-	}
-}
 
 if (this.x < 100){
     this.x = 300
